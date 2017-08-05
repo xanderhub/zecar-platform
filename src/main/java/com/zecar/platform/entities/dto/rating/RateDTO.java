5ba63256eda8c8734d211d441d49cd271c7a0378
Dmitry <dmitry.paradny@gmail.com>
2017-08-05 13:54:19 +0300

    json props

--- src/main/java/com/zecar/platform/entities/dto/rating/RateDTO.java
+++ src/main/java/com/zecar/platform/entities/dto/rating/RateDTO.java
@@ -1,10 +1,26 @@
 package com.zecar.platform.entities.dto.rating;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModelProperty;
+
 public final class RateDTO {
+	@JsonProperty(required=true)
+	@ApiModelProperty(notes="User's id.", required=true)
 	public String userId;
+	
+	@JsonProperty(required=true)
+	@ApiModelProperty(notes="Item's id.", required=true)
 	public String itemId;
+	
+	@JsonProperty(required=true)
+	@ApiModelProperty(notes="Rate value", required=true)
 	public double rating;
+	
+	@JsonProperty(required=false)
+	@ApiModelProperty(notes="This rate time", required=false)
 	public long time;
+	
 	@Override
 	public final int hashCode() {
 		final int prime = 31;
